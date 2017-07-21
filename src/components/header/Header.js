7262fd2eec91e500b1c597ffd4240794ad825fc7
Sorin Chiriac <sorin.chiriac@ig.com>
2017-07-21 17:29:03 +0100

    Added Materialize

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,20 @@ import './Header.css';
 class Header extends Component {
   render() {
     return (
-        <h1>Header</h1>
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey">
+              <div className="card-content white-text">
+                <span className="card-title">First Card</span>
+                <p>This is the first card created with Materialize.</p>
+              </div>
+              <div className="card-action">
+                <a href="#">Sweet</a>
+                <a href="#">Whatever</a>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
