7acfe32c667dcda15c742b15be9138bff2e4cc03
Sorin Chiriac <sorin.chiriac@ig.com>
2017-07-21 17:15:21 +0100

    First commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+        <h1>Header</h1>
+    );
+  }
+}
+
+export default Header;
